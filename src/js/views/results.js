e65804298e013a2980f05f919a8b994d2b642f2b
Kiriana Brown <kiriana.brown@hotmail.com>
2021-06-24 16:15:07 +1200

    add pending state and reveal computer selection with animation fadein

--- src/js/views/results.js
+++ src/js/views/results.js
@@ -1,7 +1,33 @@
-import { elements } from "./base";
+import {
+    elements
+} from "./base";
 
-export const renderPending = (playerOption) => {
-    console.log(playerOption);
+export const renderPending = (playerOption, computerOption) => {
     elements.container_controls.style.display = "none";
+
+    const html = `
+        <div class="container-pending--container">
+            <h2 class="container-pending--heading ">You Picked</h2>
+            <button class="container-pending--button ${playerOption}"><img src="img/icon-${playerOption}.svg" alt="image of ${playerOption}">  
+            </button>
+        </div> 
+        <div class="container-pending--container">
+            <h2 class="container-pending--heading ">The House Picked</h2>
+            <button class="container-pending--button pending"></button>
+        </div>     
+    `
+    const markup = `
+    <div class="container-pending--container animate-in">
+            <h2 class="container-pending--heading ">The House Picked</h2>
+            <button class="container-pending--button ${computerOption}"><img src="img/icon-${computerOption}.svg" alt="image of ${computerOption}">  
+            </button>
+        </div>
+    `
+    elements.container_pending.insertAdjacentHTML('afterbegin', html);
     elements.container_pending.style.display = "flex";
+
+    setTimeout(() => {
+        elements.container_pending.removeChild(elements.container_pending.lastElementChild);
+        elements.container_pending.insertAdjacentHTML('beforeend', markup)
+    }, 1000)
 };
