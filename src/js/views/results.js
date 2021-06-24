e3ab26d40aa20a95c901c4ee52fdb0dbce5b5ed2
KirianaBrown <kjb0369@arastudent.ac.nz>
2021-06-24 14:59:25 +1200

    flex pending container

--- src/js/views/results.js
+++ src/js/views/results.js
@@ -3,5 +3,5 @@ import { elements } from "./base";
 export const renderPending = (playerOption) => {
     console.log(playerOption);
     elements.container_controls.style.display = "none";
-    elements.container_pending.style.display = "inline";
+    elements.container_pending.style.display = "flex";
 };
