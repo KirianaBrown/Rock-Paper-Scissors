78e21e5ecd2157c638fa34f10733e79c2eaf1de5
KirianaBrown <kjb0369@arastudent.ac.nz>
2021-06-25 15:41:17 +1200

    add delay for score reveal

--- src/js/views/scores.js
+++ src/js/views/scores.js
@@ -1,9 +1,11 @@
 import { elements } from "./base";
 
 export const renderScore = (state) => {
-    elements.score.innerHTML = state.currentScore;
-    elements.score.classList.add("animate-score");
     setTimeout(() => {
-        elements.score.classList.remove("animate-score");
-    }, 1000);
+        elements.score.innerHTML = state.currentScore;
+        elements.score.classList.add("animate-score");
+        setTimeout(() => {
+            elements.score.classList.remove("animate-score");
+        }, 1000);
+    }, 2200);
 };
