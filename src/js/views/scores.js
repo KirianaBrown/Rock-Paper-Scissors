305918c90eeeacedb81a0d4d6641bd35a911f591
KirianaBrown <kjb0369@arastudent.ac.nz>
2021-06-25 16:37:18 +1200

    final changes to logic

--- src/js/views/scores.js
+++ src/js/views/scores.js
@@ -1,11 +1,15 @@
 import { elements } from "./base";
 
 export const renderScore = (state) => {
-    setTimeout(() => {
-        elements.score.innerHTML = state.currentScore;
-        elements.score.classList.add("animate-score");
+    if (!state.gameIsPlaying) {
+        elements.score.innerHTML = 0;
+    } else {
         setTimeout(() => {
-            elements.score.classList.remove("animate-score");
-        }, 1000);
-    }, 2200);
+            elements.score.innerHTML = state.currentScore;
+            elements.score.classList.add("animate-score");
+            setTimeout(() => {
+                elements.score.classList.remove("animate-score");
+            }, 1000);
+        }, 2200);
+    }
 };
