2a6a33fc76246142a52318eeda5ec6806a8c151d
KirianaBrown <kjb0369@arastudent.ac.nz>
2022-04-20 17:25:34 +1200

    update img src location for build

--- src/js/views/reset.js
+++ src/js/views/reset.js
@@ -14,9 +14,9 @@ export const resetScoreElements = () => {
     // 2. game result text
     elements.gameResult.textContent = "rock paper or scissors?";
     // 3. images
-    selectionOptionElements.playerImage.src = `../img/player.svg`;
+    selectionOptionElements.playerImage.src = `img/player.svg`;
     selectionOptionElements.playerImage.alt = `player selection`;
 
-    selectionOptionElements.computerImage.src = `../img/robot.svg`;
+    selectionOptionElements.computerImage.src = `img/robot.svg`;
     selectionOptionElements.computerImage.alt = `robot player`;
 };
